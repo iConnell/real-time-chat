af819cd7e3811452e58b2ab72e61dd5ff2bf2951
McConnell <ik.ugwuanyi@gmail.com>
2022-08-24 13:59:19 +0100

    socket.io setup

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const errorHandlerMiddleware = require("./middlewares/errorHandler");
 const authMiddleware = require("./middlewares/auth");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
 app.use(express.json());
 
 app.use("/api/auth/", authRoutes);
@@ -19,7 +25,7 @@ const port = process.env.PORT || 8000;
 const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log("Speak for the app is listening");
     });
   } catch (err) {
